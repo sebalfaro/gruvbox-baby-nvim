a1039492346ced2b6d892f59c1d6fb0163a95b2c
Sebastian Alfaro <sebastian.alfaro@ultra.me>
2023-11-25 10:05:47 -0300

    chore: adding background color

--- lua/gruvbox-baby/colors.lua
+++ lua/gruvbox-baby/colors.lua
@@ -27,11 +27,26 @@ local the_palette = {
   clean_green = "#8ec07c",
   milk = "#E7D7AD",
   none = "NONE",
+  background_colour = "#000000",
 }
 
 the_palette.term = {
-  "#282828", "#cc241d", "#98971a", "#d79921", "#458588", "#b16286", "#689d6a", "#ebdbb2",
-  "#928374", "#fb4934", "#b8bb26", "#fabd2f", "#83a598", "#d3869b", "#8ec07c", "#a89984",
+  "#282828",
+  "#cc241d",
+  "#98971a",
+  "#d79921",
+  "#458588",
+  "#b16286",
+  "#689d6a",
+  "#ebdbb2",
+  "#928374",
+  "#fb4934",
+  "#b8bb26",
+  "#fabd2f",
+  "#83a598",
+  "#d3869b",
+  "#8ec07c",
+  "#a89984",
 }
 
 -- these are backgrounds
